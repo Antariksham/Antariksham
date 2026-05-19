60f48b365debdbd202caa1c74dbe239ee3a80cf5
mayank7643 <mayankprasad7654@gmail.com>
2026-05-19 08:08:30 +0530

    Add files via upload

---
+++ app/app-page.tsx
@@ -0,0 +1,21 @@
+import type { Metadata }       from 'next'
+import { siteConfig }          from '@/config/site'
+import { HomePage }            from '@/modules/homepage/components/HomePage'
+import { getLatestArticles }   from '@/modules/news/services/getArticles'
+import { getFeaturedMissions } from '@/modules/missions/services/getMissions'
+
+export const metadata: Metadata = {
+  title:       siteConfig.seo.defaultTitle,
+  description: siteConfig.description,
+}
+
+export const revalidate = 300
+
+export default async function Page() {
+  const [articles, missions] = await Promise.all([
+    getLatestArticles(7),
+    getFeaturedMissions(4),
+  ])
+
+  return <HomePage articles={articles} missions={missions} />
+}
